adb7a1305527581cc431ba8167b2012bb566ccb7
Sindre Mehus <sm@scienta.no>
2016-02-19 19:53:30 +0100

    Increase default transcoding bitrate from 192 to 320 kbps.

--- subsonic-main/src/main/java/net/sourceforge/subsonic/service/TranscodingService.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/service/TranscodingService.java
@@ -199,7 +199,7 @@ public class TranscodingService {
         if (transcoding != null) {
             parameters.setTranscoding(transcoding);
             if (maxBitRate == null) {
-                maxBitRate = mediaFile.isVideo() ? VideoPlayerController.DEFAULT_BIT_RATE : TranscodeScheme.MAX_192.getMaxBitRate();
+                maxBitRate = mediaFile.isVideo() ? VideoPlayerController.DEFAULT_BIT_RATE : TranscodeScheme.MAX_320.getMaxBitRate();
             }
         } else if (maxBitRate != null) {
             boolean supported = isDownsamplingSupported(mediaFile);
